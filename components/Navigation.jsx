382d7844e7c776e57506fdd82a827f0fcdd3f3c6
delasimensah <mensadelasi@gmail.com>
2022-07-18 14:43:43 +0000

    added mobile navigation

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,23 +1,59 @@
-import React from "react";
+import { useState } from "react";
 import { Box, Title, Text } from "@mantine/core";
+import Image from "next/image";
+
+import { Drawer } from "@mantine/core";
+
+const Icon = ({ src }) => <Image src={src} alt="" width={20} height={20} />;
+
+const Card = ({ className, children }) => (
+  <Box className={`h-44 rounded-lg p-3 bg-white ${className}`}>{children}</Box>
+);
 
 const Navigation = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
   return (
-    <Box className="flex lg:block md:space-x-5 lg:space-x-0 lg:space-y-5 ">
-      <Box className="flex items-center flex-1 p-3 text-white bg-cover md:p-5 md:items-end md:rounded-lg bg-header-sm md:bg-header-md lg:bg-header-lg md:h-40">
-        <Box>
-          <Title className="text-white capitalize h3 md:h2 xl:h1">
+    <Box className="flex lg:block md:space-x-5 lg:space-x-0 lg:space-y-5">
+      <Box className="flex items-center justify-between h-20 px-3 bg-cover bg-header-sm z-[1000] w-full md:flex-1 md:h-44 md:bg-header-md md:rounded-lg md:p-3 md:items-end lg:bg-header-lg ">
+        <Box className="">
+          <Title className="capitalize text-ghostWhite2 h3 md:h2">
             Frontend Mentor
           </Title>
           <Text className="capitalize text-ghostWhite body2 md:body1">
             feedback board
           </Text>
         </Box>
+
+        <Box className="md:hidden" onClick={() => setMobileOpen(!mobileOpen)}>
+          {mobileOpen ? (
+            <Icon src="/assets/shared/mobile/icon-close.svg" />
+          ) : (
+            <Icon src="/assets/shared/mobile/icon-hamburger.svg" />
+          )}
+        </Box>
       </Box>
 
-      <Box className="flex-1 hidden h-40 bg-white rounded-lg md:block"></Box>
+      <Card className="hidden md:flex-1 md:block">hello</Card>
 
-      <Box className="flex-1 hidden h-40 bg-white rounded-lg md:block "></Box>
+      <Card className="hidden md:flex-1 md:block">hello</Card>
+
+      <Drawer
+        opened={mobileOpen}
+        onClose={() => setMobileOpen(false)}
+        position="right"
+        overlayOpacity={0.5}
+        size="85%"
+        withCloseButton={false}
+      >
+        <Box className="h-full p-5 mt-20 bg-ghostWhite">
+          <Box className="space-y-5">
+            <Card>hello</Card>
+
+            <Card>hello</Card>
+          </Box>
+        </Box>
+      </Drawer>
     </Box>
   );
 };
