ef0782dbccd6c2b79b483843a3a3550f3abe1e68
delasimensah <mensadelasi@gmail.com>
2022-07-21 16:11:01 +0000

    updated no suggestions component

--- components/NoSuggestions.jsx
+++ components/NoSuggestions.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mantine/core";
+import { Box, Title, Text, Button } from "@mantine/core";
 
 import { Icon } from ".";
 
@@ -9,6 +9,30 @@ const NoSuggestions = () => {
         src="/assets/suggestions/illustration-empty.svg"
         className="w-24 h-24 md:w-36 md:h-36"
       />
+
+      <Box className="flex flex-col items-center justify-center space-y-5 md:w-[58%] lg:w-[35%]">
+        <Title className="text-center h1 text-gore">
+          There is no feedback yet.
+        </Title>
+
+        <Text className="text-center body1 text-grey">
+          Got a suggestion? Found a bug that needs to be squashed? We love
+          hearing about new ideas to improve our app.
+        </Text>
+      </Box>
+
+      <Button
+        leftIcon={
+          <Icon src="/assets/shared/icon-plus.svg" className="w-3 h-3" />
+        }
+        variant="filled"
+        classNames={{
+          root: "w-36 text-xs lg:w-40 lg:text-sm",
+          filled: "bg-primary hover:bg-[#AF48DC]",
+        }}
+      >
+        Add Feedback
+      </Button>
     </Box>
   );
 };
