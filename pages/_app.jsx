95ded0db5302af164e050828ce84623e192d8add
delasimensah <mensadelasi@gmail.com>
2022-07-22 10:43:12 +0000

    added document component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,23 @@
 import { MantineProvider } from "@mantine/core";
+import Head from "next/head";
 import theme from "../utils/theme";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <MantineProvider theme={theme}>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <>
+      <Head>
+        <title>Product Feedback</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider theme={theme}>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
   );
 };
 
