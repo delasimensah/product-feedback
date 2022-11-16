915a523f02abc8a3fc83e854caa4b9141e89ea4a
delasimensah <mensadelasi@gmail.com>
2022-11-16 14:02:14 +0000

    updates

--- pages/roadmap.jsx
+++ pages/roadmap.jsx
@@ -31,7 +31,7 @@ const CardWrapper = ({ data, color, title }) => (
           {title}
         </Text>
 
-        <Link href={`/`}>
+        <Link href={`/feedback-details/${id}`}>
           <a className="h4 text-bay hover:text-deepBlue">{sugg.title}</a>
         </Link>
 
